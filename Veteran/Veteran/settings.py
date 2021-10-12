db9e8d3c393f14de9b8c471f5272e916cb2fb34d
jay <jaewhan232@naver.com>
2021-10-12 17:25:53 +0900

    프로젝트 디렉토리 설정 변경

--- Veteran/Veteran/settings.py
+++ Veteran/Veteran/settings.py
@@ -20,31 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os, json
-from django.core.exceptions import ImproperlyConfigured
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = 'django-insecure-h(jh3biqk_cs1r^^u)8-h94$t!_6$z15rwcgi$m29-_8a6+bah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
