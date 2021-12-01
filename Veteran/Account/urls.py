72fc5494a1fba57b1f9d9d140452d3338997e5b5
zoe <jesuiszoekim@gmail.com>
2021-12-02 03:09:31 +0900

    apply, approve

--- Veteran/Account/urls.py
+++ Veteran/Account/urls.py
@@ -6,4 +6,8 @@ app_name='Account'
 urlpatterns=[
     path('',views.login,name='login'),
     path('signup/', views.signup, name='signup'),
+    path('mypage/',views.mypage, name = 'mypage' ),
+    path('hostapply/', views.hostapply, name = 'hostapply'),
+    path('hostapply/apply', views.apply, name = 'apply'),
+    path('hostapprove/', views.hostapprove, name ='hostapprove'),
 ]
--- Veteran/Account/views.py
+++ Veteran/Account/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 
 from .models import User
 
+from .models import HostApplication
+
+
 
 # Create your views here.
 #수정필요
@@ -40,4 +43,40 @@ def signup(request):
             auth.login(request,user)
             return redirect('Game:gamelist')
     return render(request,'Account/join_page.html')
-            
+    
+def mypage(request):
+    return render(request, 'Account/myinfo.html')
+
+def hostapply(request):
+    return render(request, 'Account/Host Application.html')
+
+def apply(request):
+    host = HostApplication()
+    host.host = request.Get["username"]
+    host.group_name = request.Get["team_name"]
+    host.court_location = request.Get["field"]
+    host.intro = request.Get["intro"]
+    host.save()
+    return redirect('Account/myinfo.html')
+
+
+   
+def hostapprove(request):
+    contents = HostApplication.objects.all()
+    return render(request, 'Account/Host approval.html', {'host_list': contents})
+
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+class CreateWorkView(PermissionRequiredMixin, CreateView):
+    raise_exception = True
+    permission_required = 'main.add_work'
+    # 여러 권한
+    # permission_required = ('polls.can_open', 'polls.can_edit')
+    model = Work
+    form_class = WorkForm
+    template_name = 'main/work_form.html'
+
+    def get_success_url(self):
+        return reverse('index')
+    
