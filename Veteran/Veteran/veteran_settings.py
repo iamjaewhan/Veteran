a5e51109ff35fff1060b30808a2ed91d8d55fc8b
jay <jaewhan232@naver.com>
2022-04-01 15:14:42 +0900

    add secret_key

---
+++ Veteran/Veteran/veteran_settings.py
@@ -0,0 +1,14 @@
+VETERAN_SECRET={
+    "SECRET_KEY" : "django-insecure-h(jh3biqk_cs1r^^u)8-h94$t!_6$z15rwcgi$m29-_8a6+bah"
+}
+
+VETERAN_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'veteran',
+        'USER':'admin',
+        'PASSWORD':'veteran2021-2!',
+        'HOST':'veteran.chd9fpf6bwch.us-east-2.rds.amazonaws.com',
+        'PORT':'3306',
+    }
+}
